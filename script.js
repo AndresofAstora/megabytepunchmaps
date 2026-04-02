e4110e36d251a084d0d6a37f405f7d45c0adc458
AndresofAstora <spencerandres@hotmail.com>
2026-04-01 23:24:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,82 @@
-// Map dimensions in pixels (width, height of your image)
-const mapWidth = 3411; 
-const mapHeight = 1000;
+const mapWidth = 2000;
+const mapHeight = 1500;
 
-// Initialize map with CRS.Simple (for image coordinates)
 const map = L.map('map', {
   crs: L.CRS.Simple,
   minZoom: -2,
-  maxZoom: 2
+  maxZoom: 2,
+  zoomControl: false // disable default position
 });
 
-// Define image bounds
-const bounds = [[0,0], [1000, 3411]];
+// Move zoom to right
+L.control.zoom({ position: 'topright' }).addTo(map);
 
-// Add the custom image as the map
-L.imageOverlay('images/map-megabyte-electro1.png', bounds).addTo(map);
-
-// Set initial view to center of image
+const bounds = [[0, 0], [mapHeight, mapWidth]];
+L.imageOverlay('images/my-map.png', bounds).addTo(map);
 map.fitBounds(bounds);
 
-// Example marker positions (y, x) in pixels
+// --- Custom Icons ---
+const icon1 = L.icon({
+  iconUrl: 'images/marker-icon.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32]
+});
+
+const icon2 = L.icon({
+  iconUrl: 'images/marker-icon.png', // replace with different icon if desired
+  iconSize: [32, 32],
+  iconAnchor: [16, 32]
+});
+
+// --- Marker Groups ---
+const layerGroups = {
+  type1: L.layerGroup(),
+  type2: L.layerGroup()
+};
+
+// --- Marker Data ---
 const markers = [
-  { name: 'Start', coords: [100, 100] },
-  { name: 'End', coords: [200, 200] },
-  { name: 'Checkpoint', coords: [300, 300] },
-  { name: 'Bit Chest', coords: [400, 400] },
-  { name: 'Part Chest', coords: [500, 500] },
-  { name: 'Color Capsule', coords: [600, 600] }
+  { name: 'A', coords: [500, 400], type: 'type1', icon: icon1 },
+  { name: 'B', coords: [800, 1200], type: 'type1', icon: icon1 },
+  { name: 'C', coords: [1200, 1500], type: 'type2', icon: icon2 }
 ];
 
-// Add markers
+// Add markers to groups
 markers.forEach(m => {
-  L.marker(m.coords).addTo(map)
+  const marker = L.marker(m.coords, { icon: m.icon })
     .bindPopup(`<b>${m.name}</b>`);
+
+  layerGroups[m.type].addLayer(marker);
+});
+
+// Add all groups to map initially
+Object.values(layerGroups).forEach(group => group.addTo(map));
+
+// --- Legend Toggle Logic ---
+document.getElementById('type1').addEventListener('change', (e) => {
+  toggleLayer('type1', e.target.checked);
+});
+
+document.getElementById('type2').addEventListener('change', (e) => {
+  toggleLayer('type2', e.target.checked);
+});
+
+function toggleLayer(type, show) {
+  if (show) {
+    map.addLayer(layerGroups[type]);
+  } else {
+    map.removeLayer(layerGroups[type]);
+  }
+}
+
+// --- Toggle All Button ---
+let allVisible = true;
+
+document.getElementById('toggleAll').addEventListener('click', () => {
+  allVisible = !allVisible;
+
+  Object.keys(layerGroups).forEach(type => {
+    document.getElementById(type).checked = allVisible;
+    toggleLayer(type, allVisible);
+  });
 });
